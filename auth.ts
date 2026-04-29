46c583f8e082b28092eccf4317cc8ef13625dcf2
Shahab155 <bhuttoshahab1544@gmail.com>
2026-04-30 04:43:06 +0500

    update

--- auth.ts
+++ auth.ts
@@ -9,6 +9,13 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "select_account",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
     }),
     Credentials({
       name: "Credentials",
